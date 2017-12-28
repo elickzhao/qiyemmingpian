82e15d6549851852c0f8f3f9a509329cd85560e8
elickzhao <xwiwi@foxmail.com>
2017-12-28 23:32:28 +0800

    有logo滚动广告版

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,16 +13,12 @@ Page(Object.assign({}, Zan.NoticeBar,{
     showPopup: false,
     img: 1,
     movable: {
-      text: '足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。'
+      text: '欧美佳化妆美甲祛斑纹绣店。温州永嘉县瓯北巴黎花园D栋13号.开业酬宾,欢迎新老顾客光临'
     },
   },
   onShow() {
     // 滚动通告栏需要initScroll
     this.initZanNoticeBarScroll('movable');
-    // initScroll的通告栏如果宽度足够显示内容将保持静止
-    this.initZanNoticeBarScroll('static1');
-    // 不进行initScroll的通告栏也将保持静止
-    // this.initZanNoticeBarScroll('static2');
   },
   // 拨打电话
   call: function (e) {
